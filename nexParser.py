7c92b6077eab2510272386a292be9103d83c811d
Asira <asira@asira.dev>
2025-02-26 17:12:40 -0700

    Began working on parser (generate AST from tokens)

--- nexParser.py
+++ nexParser.py
@@ -3,7 +3,132 @@ PARSER MAKES NODES FROM TOKENS
 """
 
 
-# PARSE TOKENS from tokenizeScript) INTO ABSTRACT SYNTAX TREE
-def parseTokens(tokens):
-  return 0
 
+
+
+# what kinsd of nodes are there?
+nodeTypes = {
+  "expression",
+}
+
+
+
+
+
+class abstractSyntaxTree:
+  def __init__(self):
+    # list of dictionaries where each entry is a node
+    # contains: nodeID, nodeType
+    self.tree = []
+
+  def insert(self, nodeID, nodeType):
+    self.tree.insert(len(self.tree), {'nodeID': nodeID, 'nodeType': nodeType})
+  
+AST = abstractSyntaxTree()
+  
+
+
+
+
+
+# PARSE TOKENS from tokenizeScript) INTO ABSTRACT SYNTAX TREE
+def parseTokens(tokenStack:object) -> object:
+  currentToken:list
+  tokenLineNumber:int
+  tokenType:str
+  tokenValue:any
+
+  currentToken = None   # currentToken[0] is lineNumber, [1] is tokenType, [2] is tokenValue
+  tokenLineNumber = 0   # currentToken[0]
+  tokenType = None      # currentToken[1]
+  tokenValue = None     # currentToken[2]
+  
+  # parts of a node
+  currentNode: dict
+  nodeID:int = 1
+  nodeType:str
+  nodeRef:str
+  nodeName:str
+  nodeVal:str
+
+
+
+  # update nodeID, reset current token and node container
+  def resetForNextNode() -> None:
+    nonlocal nodeID
+    nonlocal currentToken
+    nonlocal currentNode
+
+    nodeID += 1
+    currentToken = None
+    currentNode = None
+
+
+
+  # unpack current token into parts
+  def getNextToken()->None:
+    nonlocal currentToken
+    nonlocal tokenLineNumber
+    nonlocal tokenType
+    nonlocal tokenValue
+
+    # ensure token data is clean by erasing previous
+    currentToken = None
+    tokenLineNumber = None
+    tokenType = None
+    tokenValue = None
+
+    # get the currentToken, lineNumber, type, and value
+    # remove from stack so it doesn't get processed again
+    currentToken = tokenStack.readCurrentToken()
+    tokenLineNumber = currentToken[0]
+    tokenType = currentToken[1]
+    tokenValue = currentToken[2]
+    tokenStack.pop()
+
+    
+
+  # process tokens into nodes
+  while True:
+    # all tokens have been processed and popped
+    if len(tokenStack.stack) == 0:
+      break
+
+    # run function to populate currentToken data
+    getNextToken()
+    print(currentToken)
+
+    if tokenType in ['scptStrt', 'scptEnd']:
+      AST.insert(nodeID, tokenType)
+      resetForNextNode()
+      continue
+
+    # start of an expression
+    if tokenType == 'exprStrt' and tokenValue == '@':
+      nodeType = 'expression'
+      
+      # next token should be a ref
+      getNextToken()
+      if tokenType == 'ref':
+        nodeRef = tokenValue
+      elif tokenType == 'arg':
+        nodeRef = tokenValue
+      else:
+        raise Exception('Fatal Error: Expected expression reference ')
+      
+      getNextToken()
+
+    
+      """
+      nodeID: nodeID
+      nodeType: 'expression'
+      nodeRef: 'var'
+      nodeName: 'url'
+      nodeVal: 'https://example.com/favico.ico'
+      """
+
+
+
+  print(AST.tree)
+
+    
