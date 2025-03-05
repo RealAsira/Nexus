14ed8fe972e862a3f184eeac8afee16b092dd510
Asira <asira@asira.dev>
2025-03-04 19:44:58 -0700

    Began work on parser with a better idea of what I'm doing and how to approach it

--- nexParser.py
+++ nexParser.py
@@ -1,12 +1,18 @@
 """
-PARSER MAKES NODES FROM TOKENS
+PARSER MAKES NODES FROM TOKENS AND POPULATES ABSTRACT SYNTAX TREE (AST)
 """
 
+import nexServerGlobals
+allReservedTokens = nexServerGlobals.allReservedTokens
+stringDelimTokens = nexServerGlobals.stringDelimTokens
+xmlDelimTokens = nexServerGlobals.stringDelimTokens
+refTypeTokens = nexServerGlobals.refTypeTokens
 
 
 
 
-# what kinsd of nodes are there?
+
+# what kinds of nodes are there?
 nodeTypes = {
   "expression",
 }
@@ -33,6 +39,11 @@ AST = abstractSyntaxTree()
 
 # PARSE TOKENS from tokenizeScript) INTO ABSTRACT SYNTAX TREE
 def parseTokens(tokenStack:object) -> object:
+  #global allReservedTokens
+  global refTypeTokens
+  #global stringDelimTokens
+  #global xmlDelimTokens
+
   currentToken:list
   tokenLineNumber:int
   tokenType:str
@@ -90,10 +101,464 @@ def parseTokens(tokenStack:object) -> object:
 
   # process tokens into nodes
   while True:
-    # all tokens have been processed and popped
-    if len(tokenStack.stack) == 0:
+    # if stack is empty then go on to interpretting else get the next token
+    if len(tokenStack.stack) <= 0: break
+    else: getNextToken()
+
+    # start of script
+    if tokenType == 'scptStrt':
+      print(currentToken)
+      continue
+
+    # end of script
+    elif tokenType == 'scptEnd':
+      print(currentToken)
       break
 
-    getNextToken()
+    elif tokenType != 'exprStrt':
+      print(f"tokenType of {tokenType} was not expected here")
+
+    # start of an expression
+    elif tokenType == 'exprStrt':
+      """
+      expressions require exprStrt, a ref (eg var, func), and exprEnd
+      some also have a generic name (like func name), args, types, definitions
+      """
+
+      getNextToken()  # next token will dictate how to process
+      if not tokenType in refTypeTokens:
+        #then this is a call to an expression
+        print(currentToken)
+        continue
+        ...
+      
+      else:
+        #then this is a built in expression... list of possible values:
+        #all, any, either, notAny, neither, not, iv, nv
+        #abort, stop, cookie, httpGet, httpPost, output, sleep, wait
+        #rspns_header, rspns_redir
+        #calc, min, max, chr, ord, date, now, today
+        #guid, random
+        #def, getglobal, nonlocal, print, return
+        #class, object, self
+        #library, use
+        #tern, if, switch, when, else
+        #const, var
+        match tokenValue.lower():
+          case 'all':
+            #all requires args (...) as child nodes. Search from open paren until close
+            """
+            example:
+            ref: all, args: 2+
+              returns true if all args evaluate to bool 1
+            """
+            print(currentToken)
+            continue
+
+          case 'any':
+            """
+            example:
+            ref:any, args: 2+
+              returns true if any arg is evaluated to bool 1
+            """
+            print(currentToken)
+            continue
+
+          case 'either':
+            """
+            example:
+            ref: either, args: exactly 2
+              returns true if either arg is evaluated to bool 1
+            """
+            print(currentToken)
+            continue
+
+          case 'neither':
+            """
+            example:
+            ref: neither, args: exactly 2
+              returns true if all args are evaluated to bool 0
+            """
+            print (currentToken)
+            continue
+
+          case 'notAny':
+            """
+            example:
+            ref: notany, args: 2+
+              returns true if all args are evaluated to bool 0
+            """
+            print(currentToken)
+            continue
+
+          case 'not':
+            """
+            example:
+            ref: not, args: exactly 1
+              returns inverse of bool value
+            """
+            print(currentToken)
+            continue
+
+          case 'iv':
+            """
+            example:
+            ref: iv, args: exactly 1
+              returns true if bool 1, true, or expression has a non-0, non-null, non-blank value
+            """
+            print(currentToken)
+            continue
+
+          case 'nv':
+            """
+            example:
+            ref: nv, args: exactly 1
+              returns true if bool 0, false, or expression has a 0, null, blank value
+            """
+            print(currentToken)
+            continue
+
+          case 'abort':
+            """
+            example:
+            ref: abort, args: 0
+              aborts response and doesn't send it
+            """
+            print(currentToken)
+            continue
+
+          case 'stop':
+            """
+            example:
+            ref: stop, args: 0
+              stops response and sends what is already available
+            """
+            print(currentToken)
+            continue
+
+          case 'cookie':
+            """
+            example:
+            ref: cookie, args: up to 8, x required (name:str, value:str, domain:str, path:str, expires:datetime, httponly:bool, secure:bool, samesite:str)
+              sends a cookie to the client
+            """
+            print(currentToken)
+            continue
+
+          case 'httpGet':
+            """
+            example:
+            ref: httpGet, args: exactly 1 (url:str)
+              gets data from another server ..
+              .. assign as a value to a variable to use (@var result = @httpGet(...))
+            """
+            print(currentToken)
+            continue
+
+          case 'httpPost':
+            """
+            example:
+            ref: httpPost, args: exactly 1 (url:str)
+              posts data to another server .. no return value
+            """
+            print(currentToken)
+            continue
+
+          case 'output':
+            """
+            example:
+            ref: output, args: exactly 1 (responseContent:str)
+              what the server's response content is
+            """
+            print(currentToken)
+            continue
+
+          case 'sleep':
+            """
+            example:
+            ref: sleep, args: exactly 1 (time in sec:number)
+              time before continuing this response
+            """
+            print(currentToken)
+            continue
+
+          case 'wait':
+            """
+            example:
+            ref: wait, args: exactly 1 (time in sec:number)
+              time before server will continue responding
+              PAUSES ALL RESPONSES
+            """
+            print(currentToken)
+            continue
+
+          case 'rspns_header':
+            """
+            example:
+            ref: rspns_header, args: exactly 2 (headerName:str, headerValue:str)
+              add a header to the response
+            """
+            print(currentToken)
+            continue
+
+          case 'rspns_redir':
+            """
+            example:
+            ref:rspns_redir, args: exactly 1 (url:str)
+              redirect client to new page
+            """
+            print(currentToken)
+            continue
+
+          case 'calc':
+            """
+            example:
+            ref:calc, args 2+ (numericalLiterals, operators, refs)
+              calculates the value of a mathematical or binary expression
+            """
+            print(currentToken)
+            continue
+
+          case 'min':
+            """
+            example:
+            ref:min, args 2+ (numericalLiterals)
+              returns smallest value
+            """
+            print(currentToken)
+            continue
+
+          case 'max':
+            """
+            example:
+            ref:max, args 2+ (numericalLiterals)
+              returns largest value
+            """
+            print(currentToken)
+            continue
+
+          case 'chr':
+            """
+            example:
+            ref:chr, args: exactly 1 (numericLiteral ASCII table)
+              returns the ordinal value of an ASCII character ie @chr(64) -> @
+            """
+            print(currentToken)
+            continue
+
+          case 'ord':
+            """
+            example:
+            ref:ord, args: exactly 1 (alphaLiteral in ASCII table)
+              returns the character value of an ASCII ordinal ie @chr(|) -> 124
+            """
+            print(currentToken)
+            continue
+
+          case 'date':
+            """
+            example:
+            ref:date, args: 0-2 (date:str, time:str)
+              0 args - acts as @now()
+              1-2 args: returns datetime as float value
+            """
+            print(currentToken)
+            continue
+            
+          case 'now':
+            """
+            example:
+            ref:now, args: 0
+              returns right now's datetime as float value
+            """
+            print(currentToken)
+            continue
+
+          case 'today':
+            """
+            example:
+            ref:today, args: 0
+              returns today's date, at 00:00:00:000, as float value
+            """
+            print(currentToken)
+            continue
+
+          case 'guid':
+            """
+            example:
+            ref:guid, args: 0-1 (seed:str:number)
+              returns a random alphanumeric string xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx
+            """
+            print(currentToken)
+            continue
+
+          case 'random':
+            """
+            example:
+            ref:random, args: 0-1 (limit:int:float)
+              0 args - random number up to 2,147,483,647 (note that ints can be larger than signed 32-bit)
+              1 arg - random number up to that value, including same number of decimal points
+            """
+            print(currentToken)
+            continue
+
+          case 'def':
+            """
+            example:
+            ref:def, name:str, args: 0-many (vars:any), returnType:any, definition
+              this is a function
+            """
+            print(currentToken)
+            continue
+
+          case 'global':
+            """
+            example:
+            ref:getglobal, args: 1 (:ref to global var)
+              allows use of global variable in a func/method
+            """
+            print(currentToken)
+            continue
+
+          case 'nonlocal':
+            """
+            example:
+            ref:nonlocal, args: 1 (:ref to var one scope up)
+              allows use of a variable at one scope up in a func/method (cascades)
+            """
+            print(currentToken)
+            continue
+
+          case 'print':
+            """
+            example:
+            ref:print, args: 1 (value:any)
+              prints value to terminal cast as string
+            """
+            print(currentToken)
+            continue
+
+          case 'return':
+            """
+            example:
+            ref:return, args: 1 (value:any)
+              returns value as literal:type from func/method to program
+            """
+            print(currentToken)
+            continue
+
+          case 'class':
+            """
+            example:
+            UNFINISHED
+            """
+            print(currentToken)
+            continue
+
+          case 'object':
+            """
+            example:
+            UNFINISHED
+            """
+            print(currentToken)
+            continue
+
+          case 'self':
+            """
+            example:
+            ref:self, args: 0
+              reference to containing class
+            """
+            print(currentToken)
+            continue
+
+          case 'library':
+            """
+            example:
+            ref:libary, args: 1 (path:str)
+              use the modules in this library as part of program
+            """
+            print(currentToken)
+            continue
+
+          case 'use':
+            """
+            example:
+            ref:use, args:1 (moduleName:str)
+              goto/use this module here
+            """
+            print(currentToken)
+            continue
+
+          case 'tern':
+            """
+            example:
+            ref:tern, args: 2-3 (conditional, returnVal if true, returnVal if false), returnType:type
+              returns one value or another depending on conditional result
+            """
+            print(currentToken)
+            continue
+            
+          case 'if':
+            """
+            example:
+            ref:if, args: 1 (conditional), definition
+              execute definition if conditional is true
+            """
+            print(currentToken)
+            continue
+
+          case 'switch':
+            """
+            example:
+            ref:switch, args 1 (expression), definition (when/else expressions)
+              execute one of the definition depending on resulting value of expression
+            """
+            print(currentToken)
+            continue
+
+          case 'when':
+            """
+            example:
+            ref:when, args 1 (value:any), definition
+              execute this definition when value matches that of parent switch
+            """
+            print(currentToken)
+            continue
+
+          case 'else':
+            """
+            example:
+            ref:else, args 0, definition
+              execute this definition when no previous when statements nested in same parent switch definition match switch expression value
+            """
+            print(currentToken)
+            continue
+
+          case 'var':
+            """
+            example:
+            ref:var, name:str, type:any, args:none, value:any (must match type unless any)
+              assign mutable value (as literal or reference) to a variable
+              if type is any, the mutable type, if not any, then type is not mutable
+            """
+            print(currentToken)
+            continue
+
+          case 'const':
+            """
+            example:
+            ref:const, name:str, type:any (except any), value:any (must match type)
+              assign immutable value (as literal or reference) to a constant
+              type and value are both immutable once assigned
+            """
+            print(currentToken)
+            continue
+
+          case _:
+            print(f"Parser Warning A - {currentToken} (not implement in parser yet!)")
+            #raise Exception ("Fatal Error: AST Parser found reserved keyword that does not have logic programmed yet.")
+
+
     print(currentToken)
     
