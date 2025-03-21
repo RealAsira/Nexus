3201a518f43d9552a3d415f459fbaeca4cfdcb4b
Talia Shaffer <shaf.talia1@gmail.com>
2025-03-21 17:18:03 -0600

    Improved upon initial version of interpreter. Now traverses the nodes in the correct order.

--- nexInterpreter.py
+++ nexInterpreter.py
@@ -16,20 +16,6 @@ content:str = None # CONTENT INTERPRETED!!
 
 
 
-def traverseAST(nodeID, tree, depth=0):
-  if nodeID not in tree:
-    return
-  
-  node = tree[nodeID]
-  
-  for childNodeID in sorted(node["nodeBody"].keys(), key=int):
-    traverseAST(childNodeID, node["nodeBody"], depth+1)
-
-  if node["nodeType"] != 'EXPR':
-    print(f"Node {nodeID}: type = {node["nodeType"]}, ref = {node["nodeRef"]}, name = {node["nodeName"]}, line = {node['nodeLineNumber']}")
-
-
-
 def interpretAST(AST:object)->str:
   """USES AST TO GENERATE AN OUTPUT"""
   #global allReservedTokens
@@ -38,9 +24,26 @@ def interpretAST(AST:object)->str:
   #global xmlDelimTokens
   #global refTokens
 
-  nodeID:int = next(iter(AST.tree.keys()))
-  traverseAST(nodeID, AST.tree)
-  
+
+  def processNode(subAST, nodeID):
+    """Processes a node's functionality"""
+
+    nodeType = subAST["nodeType"]
+    nodeRef = subAST["nodeRef"]
+    nodeName = subAST["nodeName"]
+    nodeArgs = subAST["nodeArgs"]
+    nodeBody = subAST["nodeBody"]
+
+    for childNodeID in sorted(nodeBody.keys(), key=int):
+      processNode(nodeBody[childNodeID], childNodeID)  # use current node to extract a sub/derived AST
+
+    print(nodeID, nodeType, nodeRef, nodeName, nodeArgs)
+    return
+
+
+  nodeID:int = next(iter(AST.tree.keys()))  # get first nodeID (entry point)
+  processNode(AST.tree[nodeID], nodeID) # iterative processor entry point ... use full AST and root nodeID
+ 
   
   print(json.dumps(AST.tree, indent=2))
   return(json.dumps(AST.tree, indent=2))
