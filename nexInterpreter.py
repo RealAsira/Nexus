f1b348f9e5b7db1a1b01f9fa44b33a96e68b1f56
Asira <asira@asira.dev>
2025-03-21 20:02:31 -0600

    Further work on interpreter.

--- nexInterpreter.py
+++ nexInterpreter.py
@@ -12,7 +12,11 @@ stringDelimTokens = nexServerGlobals.stringDelimTokens
 xmlDelimTokens = nexServerGlobals.stringDelimTokens
 refTokens = nexServerGlobals.refTokens
 
+variables:list = [] # a list of references and values
+
 content:str = None # CONTENT INTERPRETED!!
+
+
 
 
 
@@ -25,25 +29,106 @@ def interpretAST(AST:object)->str:
   #global refTokens
 
 
-  def processNode(subAST, nodeID):
+  def interpretNode(node:dict, nodeID:int, childReturns:list)->any:
+    """Interprets the node (functionality)"""
+    global variables
+
+    nodeType = node["nodeType"]
+    nodeRef = node["nodeRef"]
+    nodeName = node["nodeName"]
+    nodeArgs = node["nodeArgs"]
+
+    print(nodeID, nodeType, nodeRef, nodeName, nodeArgs, childReturns)
+
+    if nodeType == "ROOT":
+      """Functionality for root of AST"""
+      ...
+
+
+
+    elif nodeType == "EXPR":
+      """Functionality for an expression call"""
+      ...
+
+
+
+    elif nodeType == "STRLITERAL":
+      """
+      Functionality for a string literal
+      returns a string from concatted child elements
+      """
+      returnVal:str = ''
+
+      for item in childReturns:
+        returnVal = f"{returnVal}{item}"
+
+      print('STRLITERAL:', returnVal)
+      return(returnVal)
+
+
+
+    elif nodeType == "REF":
+      """
+      Functionality for an expression
+      List of built-in ref-typed expressions
+      iv, nv, abort, stop, cookie, httpget, httppost, output, sleep, wait, rspns_header, rspns_redir,
+      calc, min, max, chr, ord, date, now, today, guid, random,
+      def, getglobal, nonlocal, print, return, class, object, self, library, use
+      tern, if, switch, when, else, const, var
+      """
+
+
+      if nodeRef == "VAR":
+        """handle variable declaration"""
+        ...
+
+
+
+    elif nodeType == "OP":
+      """Funtionality for an operator"""
+      ...
+
+
+
+    elif nodeType == "ARG":
+      """
+      Functionality for a arg literal
+      returns raw data literal
+      """
+      print('ARG:', nodeArgs['value'])
+      return(nodeArgs['value'])
+
+
+
+    elif nodeType == "METHOD":
+      """Functionality for a method"""
+      #HYPOTHETHICALLY THIS IS NEVER CALLED...?
+      ...
+
+
+
+    else:
+      """Catch all ... shouldn't ever be called"""
+      ...
+
+    
+
+  def processNode(subAST:dict, nodeID:int):
     """Processes a node's functionality"""
+    childReturns:list = []
 
-    nodeType = subAST["nodeType"]
-    nodeRef = subAST["nodeRef"]
-    nodeName = subAST["nodeName"]
-    nodeArgs = subAST["nodeArgs"]
-    nodeBody = subAST["nodeBody"]
+    for childNodeID in sorted(subAST["nodeBody"].keys(), key=int):
+      childReturn = processNode(subAST["nodeBody"][childNodeID], childNodeID) # get children return value
+      childReturns.append(childReturn) # append to list for parent node to process
 
-    for childNodeID in sorted(nodeBody.keys(), key=int):
-      processNode(nodeBody[childNodeID], childNodeID)  # use current node to extract a sub/derived AST
-
-    print(nodeID, nodeType, nodeRef, nodeName, nodeArgs)
-    return
+    return (interpretNode(subAST, nodeID, childReturns))
 
 
+
+  # ENTRY POINT
   nodeID:int = next(iter(AST.tree.keys()))  # get first nodeID (entry point)
   processNode(AST.tree[nodeID], nodeID) # iterative processor entry point ... use full AST and root nodeID
- 
   
+  # EXIT POINT
   print(json.dumps(AST.tree, indent=2))
   return(json.dumps(AST.tree, indent=2))
