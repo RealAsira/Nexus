23700d731287ad8af4d13dbcee0037234cb85cc3
Talia Shaffer <shaf.talia1@gmail.com>
2025-05-28 13:03:06 -0600

    Significant improvement on error/warning handling (still incomplete) ... added support for constants to interpreter

---
+++ nexErrHandler.py
@@ -0,0 +1,46 @@
+"""
+This module uses custom error handling for Nexus
+
+fatal False = warning, True = fatal error
+moduleName is the nexus (.nex) file
+lineNumber is the line number in the .nex file where the error occurred
+
+example:
+
+import nexErrHandler as neh
+try: raise neh.nexException('[errorMessage]')
+except neh.nexException as err: neh.nexError(err, [fatal], '[moduleName]', [lineNumber]) 
+"""
+
+
+
+fatalError:str = ''     # if value is assigned, stop interpreting and return this value only
+warnings:list = []      # a list of warnings that may not be fatal ... print to console
+
+
+
+class nexException(Exception):
+  def __init__(self, msg):
+    """child of Exception class ... eg, raise Exception('errmsg')"""
+    super().__init__(msg)
+
+  
+
+def nexError(err:str, fatal=False, moduleName:str='unidentified', lineNumber:int=0):
+  """call on nexus exception to handle"""
+  global warnings, fatalError
+
+  # ensure proper debug-naming
+  if moduleName in [None, '']:
+    moduleName = 'unidentified'
+  
+  if not fatal:
+    warnings.append(f'[WARNING | {moduleName.upper()}.NEX | LN {lineNumber}] {err}.')
+
+  if fatal:
+    fatalError = f'[ERROR | {moduleName.upper()}.NEX | LN {lineNumber}] {err}.'
+    print(fatalError)
+
+    # this is because error handling hasn't been built into the nexus language itself, yet
+    # some level of handling needs to be added to the language before it can be properly handled here
+    print('FATAL WARNING: Fatal Errors do NOT stop execution of script')
